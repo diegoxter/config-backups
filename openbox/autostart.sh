ca01c07663e6ec9ad70d003f11350340460fdc7b
Diego Jose <diegoxter1006@gmail.com>
2011-05-13 10:52:17 -0430

    getting started

---
+++ openbox/autostart.sh
@@ -0,0 +1,32 @@
+# This shell script is run before Openbox launches.
+# Environment variables set here are passed to the Openbox session.
+
+# D-bus
+if which dbus-launch >/dev/null 2>&1 && test -z "$DBUS_SESSION_BUS_ADDRESS"; then
+       eval `dbus-launch --sh-syntax --exit-with-session`
+fi
+
+# Run XDG autostart things.  By default don't run anything desktop-specific
+# See xdg-autostart --help more info
+#DESKTOP_ENV="GNOME"
+#if which /usr/lib/openbox/xdg-autostart >/dev/null 2>&1; then
+#  /usr/lib/openbox/xdg-autostart $DESKTOP_ENV
+#fi
+
+#Auto inicio
+sleep 19s && wicd-client &
+sleep 9s && xscreensaver -no-splash &
+sleep 8s && kupfer --no-splash &
+#sleep 8s && easystroke &
+#sleep 7s && bmpanel2 --theme=auriel-green-opaque &
+sleep 6s && clipit &
+#sleep 5s &&  trayer --align right --edge bottom --expand false --width 18 &
+sleep 6s && xrdb -merge ~/.Xresources &
+sleep 5s && urxvtd -f & 
+sleep 4s && adeskbar &
+sleep 4s && lxpanel &
+sleep 3s && conky -c .conkyplacid &
+sleep 3s && tint2 -c .config/tint2/tintarch &
+sleep 2s && cairo-compmgr &
+nitrogen --restore &
+
---
+++ vimrc
@@ -0,0 +1,37 @@
+set nocompatible
+:set spell
+:set spelllang=es,en
+:set spellsuggest=5
+:set spellfile=~/.vim/dict.add
+colorscheme biogoo
+set noerrorbells
+set visualbell t_vb=
+if has("autocmd")
+    autocmd GUIEnter * set visualbell t_vb=
+endif
+
+set noswapfile
+
+" searching
+set hlsearch            
+set incsearch          
+set ignorecase         
+set smartcase           
+
+"Columnas y Filas
+:set cursorline
+:highlight CursorLine term=underline cterm=underline guibg=Grey90    
+:set cursorcolumn
+:highlight CursorColumn term=reverse ctermbg=7 guibg=Grey90
+
+"Lineas 
+":set number
+":set numberwidth=3
+
+"Barra
+:set statusline=%F%m%r%h%w\ [FORMAT=%{&ff}]\ [POS=%04l,%04v]\ [%p%%]\ [TYPE=%Y]
+:set laststatus=2
+
+set mouse=a
+set complete+=k 
+set clipboard+=unnamed
