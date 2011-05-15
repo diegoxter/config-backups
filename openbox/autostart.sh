cd213aee2570ce03fda01016d5fb7fd29d2a820c
Diego Jose <diegoxter1006@gmail.com>
2011-05-15 03:12:04 -0430

    openbox stuff

--- openbox/autostart.sh
+++ openbox/autostart.sh
@@ -23,10 +23,11 @@ sleep 6s && clipit &
 #sleep 5s &&  trayer --align right --edge bottom --expand false --width 18 &
 sleep 6s && xrdb -merge ~/.Xresources &
 sleep 5s && urxvtd -f & 
-sleep 4s && adeskbar &
-sleep 4s && lxpanel &
-sleep 3s && conky -c .conkyplacid &
-sleep 3s && tint2 -c .config/tint2/tintarch &
+#sleep 4s && kiba-dock &
+#sleep 3s && conky -c .conkyplacid &
+#sleep 3s && tint2 -c .config/tint2/tinthotd &
+sleep 3s && bmpanel2 &
 sleep 2s && cairo-compmgr &
-nitrogen --restore &
-
+sleep 4s && thunar --daemon &
+#nitrogen --restore &
+hsetroot -fill Dropbox/Photos/Wallpapers/Blood/HIGHSCHOOL_OF_THE_DEAD_Chibi_Wallpaper__yvt2.jpg &
