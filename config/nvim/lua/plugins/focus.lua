0627af8d6ebf0d474b3cf027dec663b59be7b745
diegoxter <diegoxter1006@gmail.com>
2026-04-26 12:23:49 -0300

    Arreglar indentacion

--- config/nvim/lua/plugins/focus.lua
+++ config/nvim/lua/plugins/focus.lua
@@ -1,12 +1,12 @@
 return {
   "nvim-focus/focus.nvim",
-   opts = {
-      autoresize = {
-        enable = true,
-        width = 0,
-        height = 0,
-        minwidth = 25,
-        minheight = 8,
+  opts = {
+    autoresize = {
+      enable = true,
+      width = 0,
+      height = 0,
+      minwidth = 25,
+      minheight = 8,
       },
     },
 }
