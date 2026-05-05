4f73ac2798a21aee37884bd7ad81cf561c278ad7
diegoxter <diegoxter1006@gmail.com>
2026-05-05 19:02:07 -0300

    feat/soporte de clangd esp32

--- config/nvim/lua/plugins/astrolsp.lua
+++ config/nvim/lua/plugins/astrolsp.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroLSP allows you to customize the features in AstroNvim's LSP configuration engine
 -- Configuration documentation can be found with `:h astrolsp`
@@ -39,6 +39,12 @@ return {
     },
     -- enable servers that you already have installed without mason
     servers = {
+      clangd = {
+        cmd = {
+          "clangd",
+          "--compile-commands-dir=build",
+        },
+      },
       -- "pyright"
     },
     -- customize language server configuration passed to `vim.lsp.config`
